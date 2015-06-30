fa24b4848056a71d3f523d6dd39bd8041192fa7c
Eric Roos <ericroos13@gmail.com>
2015-06-29 23:19:47 -0500

    ok sweet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627052349) do
+ActiveRecord::Schema.define(version: 20150630031256) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",      limit: 255
